d6aa751e6b5d9b7058d1d4d7801e9e52044acd2d
emkeator <emkeator@gmail.com>
2017-08-14 10:17:30 -0600

    refactor: Change to HashRouter from BrowserRouter

--- src/router.js
+++ src/router.js
@@ -7,9 +7,9 @@ import Visit from './Components/Visit/Visit';
 
 export default (
     <Switch>
-        <Route path='lonely-mountain-winery-project-site/' exact component={Home}/>
-        <Route path='lonely-mountain-winery-project-site/wines' component={Wines}/>
-        <Route path='lonely-mountain-winery-project-site/events' component={Events}/>
-        <Route path='lonely-mountain-winery-project-site/visit' component={Visit}/>                
+        <Route path='/' exact component={Home}/>
+        <Route path='/wines' component={Wines}/>
+        <Route path='/events' component={Events}/>
+        <Route path='/visit' component={Visit}/>                
     </Switch>
 );
